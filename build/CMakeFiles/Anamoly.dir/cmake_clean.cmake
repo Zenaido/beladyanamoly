0551c29051b74ccf3b81d83ab3dbcd3ff87c117f
Zenaido <luis.hernandez@aggiemail.usu.edu>
2017-11-30 11:43:40 -0700

    added build

---
+++ build/CMakeFiles/Anamoly.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Anamoly.dir/main.cpp.o"
+  "CMakeFiles/Anamoly.dir/FrameArchitecture.cpp.o"
+  "Anamoly.pdb"
+  "Anamoly"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Anamoly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
